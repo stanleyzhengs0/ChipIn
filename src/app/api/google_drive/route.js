37632ab9aa124ca3edb33e827278fedafc77ad81
Stanley Zheng <stanleyzhengs@gmail.com>
2024-11-25 18:05:02 -0500

    initalze project

---
+++ src/app/api/google_drive/route.js
@@ -0,0 +1,11 @@
+import { NextResponse } from "next/server";
+
+export async function GET(){
+    try{
+
+        let response = await fetch(`${process.env.GOOGLE_DRIVE_API_KEY}`)
+
+    }catch(error){
+
+    }
+}
